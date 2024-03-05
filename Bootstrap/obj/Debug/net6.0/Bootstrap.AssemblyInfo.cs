007eb4d76a9a208f3283d2c5b15d9c96ad44515a
yamilever <yamilarguedas19@gmail.com>
2024-03-05 13:59:15 -0600

    columna V1

--- Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
+++ Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e79e68b6c96079188eee569166269ed20c7b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3eefe3bb227be027c7c3894cf102d9ceea5fda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
